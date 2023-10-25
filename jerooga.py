e39085ff1a50dfd4417a69eb2917c822a254559f
Zack McKenzie <zackmc0902@proton.me>
2023-10-25 15:33:16 -0500

    huh

--- jerooga.py
+++ jerooga.py
@@ -1,17 +1,5 @@
 import pygame
-
-# Texture dictionary
-type2Texture = {
-    "collision": pygame.image.load("Textures/Collision.gif"),
-    "flower": pygame.image.load("Textures/Flower.gif"),
-    "trapped": pygame.image.load("Textures/Trapped.gif"),
-    "wet": pygame.image.load("Textures/Wet.gif"),
-    "collision": pygame.image.load("Textures/Collision.gif"),
-    "water": pygame.image.load("Textures/Water.gif"),
-    "land": pygame.image.load("Textures/Land.gif"),
-    "net": pygame.image.load("Textures/Net.gif")
-    }
-type2Texture.update({f"{n}{d}{f}": pygame.image.load(f"Textures/{n}{d}{f}.gif") for n in range(4) for d in "NESW" for f in ("","_F")})
+from Tile import Tile
 
 class Jerooga:
     def __init__(self, screenWidth=644, screenHeight=644):
@@ -35,11 +23,42 @@ class Jerooga:
 
     def board_to_string(self):
         return [str(self.board[x][y]) for x in range(self.blocksWide) for y in range(self.blocksHigh)]
+        
+    def updateWindow(self):
+        # Draws tiles
+        for y in self.board:
+            for tile in y:
+                tile.draw(self.window)
+        
+        # Draws Jeroos
+        for jerooIter in self.jeroos:
+            jerooIter.draw(self.window)
+        pass
+
+
+
+
+
+
+
+   
+    
+
+
+
+
+
+
+
+
 
 # Tile class to be stored in the board 2d list, it should represent each type of block
 class Tile:
-    def __init__(self, state = "water"):
+    def __init__(self, blockX, blockY, state = "water"):
         self.state = state
+        self.blockX = blockX
+        self.blockY = blockY
+        self.img = type2Texture[self.state]
 
     # Returns pygame texture
     def getTexture(self):
@@ -50,27 +69,73 @@ class Tile:
 
     getState = __str__
     
+    def draw(self, window):
+        window.blit(self.getTexture(), (self.blockX*, self.yPos))
+    
+    def getRect(self):
+        rect = self.img.get_rect()
+        return (self.x, self.y, rect[2], rect[3])
+    
 class Jeroo(Tile):
-    def __init__(self, number=0, spawnPosition="Placeholder, correct later"):
+    def __init__(self, number, spawnPosition="Placeholder, correct later", flowers = 0):
         self.number = number
+        self.flowers = flowers
         self.spawnPosition = spawnPosition
+        self.direction = "E"
+
+    def getTexture(self):
+        if self.board[self.blockY][self.blockX].getState() == "flower":
+            return type2Texture[f"{self.number}{self.direction}_F"]
+        return type2Texture[f"{self.number}{self.direction}"]
+            
     
     # Action methods
-    def hop(n=1):
+    def hop(numberOfSpaces=1):
         pass
-    
+        # Picks up the flower on the current position and places it in inventory
     def pick():
         pass
-    
-    def plant():
+
+        # Places flower at current position
+    def plant(self, window):
+        #= Tile("Flower")
         pass
     
+        # Places a flower one block in the direction that the jeroo is facing
     def toss():
         pass
     
     def give():
         pass
 
-    def turn(direciton):
-        pass
-  
+    def turn(direction):
+        pass  
+
+    # Boolean Methods (not by zack "BooleanMaster" McKenzie)
+    
+    def hasFlower(self):
+        return True if self.flowers > 0 else False
+    
+    def isFacing(self, direction):
+        return True if self.direction == direction else False
+
+
+
+
+
+
+
+
+# Texture dictionary
+type2Texture = {
+    "collision": pygame.image.load("Textures/Collision.gif"),
+    "flower": pygame.image.load("Textures/Flower.gif"),
+    "trapped": pygame.image.load("Textures/Trapped.gif"),
+    "wet": pygame.image.load("Textures/Wet.gif"),
+    "collision": pygame.image.load("Textures/Collision.gif"),
+    "water": pygame.image.load("Textures/Water.gif"),
+    "land": pygame.image.load("Textures/Land.gif"),
+    "net": pygame.image.load("Textures/Net.gif")
+    }
+# Makes the textures for the jeroos with various flowers and directions
+type2Texture.update({f"{n}{d}{f}": pygame.image.load(f"Textures/{n}{d}{f}.gif") for n in range(4) for d in "NESW" for f in ("","_F")})
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from jerooga import Jerooga
+from Jerooga import Jerooga
 jerooga_obj = Jerooga()
 
-print(jerooga_obj.board_to_string())
+print(f"[{len(jerooga_obj.board)}][{len(jerooga_obj.board[1])}]")
 
 input("Enter To continue")
