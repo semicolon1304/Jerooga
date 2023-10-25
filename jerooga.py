f7eeed4d7bbdf2ed99ce3351a7538700f0ea7a63
Zack McKenzie <zackmc0902@proton.me>
2023-10-25 12:50:11 -0500

    Did some work. I will not explain what kind

--- jerooga.py
+++ jerooga.py
@@ -11,6 +11,7 @@ type2Texture = {
     "land": pygame.image.load("Textures/Land.gif"),
     "net": pygame.image.load("Textures/Net.gif")
     }
+type2Texture.update({f"{n}{d}{f}": pygame.image.load(f"Textures/{n}{d}{f}.gif") for n in range(4) for d in "NESW" for f in ("","_F")})
 
 class Jerooga:
     def __init__(self, screenWidth=644, screenHeight=644):
@@ -19,9 +20,11 @@ class Jerooga:
         self.screenWidth = screenWidth
         self.screenHeight = screenHeight
         self.screenSize = (screenWidth, screenHeight)
+        self.blocksWide = screenWidth // self.pixelsPerBlock
+        self.blocksHigh = screenHeight // self.pixelsPerBlock
 
         # Board is the play area, it is a 2d list, screenWidth//pixelsPerBlock x screenHeight//pixelsPerBlock
-        board = [[Tile() for x in range(screenWidth // self.pixelsPerBlock)] for y in range(screenHeight // self.pixelsPerBlock)]
+        self.board = [[Tile() for x in range(self.blocksWide)] for y in range(self.blocksHigh)]
 
         # Jeroos should be stored in a list for easy iteration in loops
         self.jeroos = []
@@ -29,6 +32,9 @@ class Jerooga:
         # Pygame will initialize in the init for Jerooga, the screen will be updated each time something on the board is changed, for instance moving a jeroo
         pygame.init()
         self.window = pygame.display.set_mode(self.screenSize)
+
+    def board_to_string(self):
+        return [str(self.board[x][y]) for x in range(self.blocksWide) for y in range(self.blocksHigh)]
 
 # Tile class to be stored in the board 2d list, it should represent each type of block
 class Tile:
@@ -38,7 +44,33 @@ class Tile:
     # Returns pygame texture
     def getTexture(self):
         return type2Texture[self.state]
+
+    def __str__(self):
+        return self.state
+
+    getState = __str__
     
 class Jeroo(Tile):
-    # TODO: Write this class
-    pass
+    def __init__(self, number=0, spawnPosition="Placeholder, correct later"):
+        self.number = number
+        self.spawnPosition = spawnPosition
+    
+    # Action methods
+    def hop(n=1):
+        pass
+    
+    def pick():
+        pass
+    
+    def plant():
+        pass
+    
+    def toss():
+        pass
+    
+    def give():
+        pass
+
+    def turn(direciton):
+        pass
+  
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from jerooga import Jerooga
 jerooga_obj = Jerooga()
 
-
+print(jerooga_obj.board_to_string())
 
 input("Enter To continue")
